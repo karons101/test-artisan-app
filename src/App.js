f0976bfa3154335ea3c4895c7ad0d4a277a92ff4
Oghenekaro <karonstical@gmail.com>
2023-10-06 23:09:38 +0100

    Make It Work

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/pages/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import { Routes, Route } from 'react-router-dom';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
