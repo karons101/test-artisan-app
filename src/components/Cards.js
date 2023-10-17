0d078cfaef5ca4f1f94693271d272e3d4afc9138
Oghenekaro <karonstical@gmail.com>
2023-10-17 02:18:08 +0100

    Sails Up

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,44 +1,45 @@
 import React from 'react';
+
 import './Cards.css';
 import CardItem from './CardItem';
 
 function Cards() {
   return (
     <div className='cards'>
-      <h1>Check out these EPIC Destinations!</h1>
+      <h1>Select a Handy Man!</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-9.jpg'
-              text='Explore the hidden waterfall deep inside the Amazon Jungle'
-              label='Adventure'
+              src={require('../images/HandyMan-1.jpg')}
+              text='Capentry at its best'
+              label='Capenter'
               path='/services'
             />
             <CardItem
-              src='images/img-2.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
+              src={require('../images/ServiceMan-2.jpg')}
+              text='Professional Service hands'
+              label='Service Man'
               path='/services'
             />
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-3.jpg'
-              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-              label='Mystery'
+              src={require('../images/Plumber-3.jpg')}
+              text='Super deal Plumbers'
+              label='Plumber'
               path='/services'
             />
             <CardItem
-              src='images/img-4.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
-              label='Adventure'
+              src={require('../images/Technician_Electrician-4.jpg')}
+              text='Experienced and Advance Tech Repairers'
+              label='Generator Repairer/ Electricians'
               path='/products'
             />
             <CardItem
-              src='images/img-8.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
-              label='Adrenaline'
+              src={require('../images/Driver-5.jpg')}
+              text='Professional Chuffuers, Drivers'
+              label='Driver'
               path='/sign-up'
             />
           </ul>
