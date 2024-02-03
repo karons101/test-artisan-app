02f7783d1bec39a1016ed72c75aa7cd39a414e36
unknown <karonstical@gmail.com>
2024-02-02 22:44:16 -0800

    Sailing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
     <div className='footer-container'>
       <section className='footer-subscription'>
         <p className='footer-subscription-heading'>
-          Join the ARTISAN newsletter to receive more informations and updates.
+          Join the UyoyoFarm newsletter to receive more informations and updates.
         </p>
         <p className='footer-subscription-text'>
           You can unsubscribe at any time.
@@ -64,11 +64,11 @@ function Footer() {
         <div class='social-media-wrap'>
           <div class='footer-logo'>
             <Link to='/' className='social-logo'>
-              ARTISAN
+              UyoyoFarm
               <i class='fab fa-typo3' />
             </Link>
           </div>
-          <small class='website-rights'>ARTISAN-Project © 2024</small>
+          <small class='website-rights'>UyoyoFarm-Project © 2024</small>
           <div class='social-icons'>
             <Link
               class='social-icon-link facebook'
